2fb018c4df6f3b4463cb71d17e62169e57e813dd
Rebecca Lötscher <rebeccaloetscher@hotmail.com>
2018-01-21 20:06:21 +0100

    funct: add map and initial location

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var initalLocation = {
+  lat: 47.050793,
+  lng: 8.310826,
+  content: "Main Station"
+}
+
+var locations = [
+  {
+    lat: 47.050793,
+    lng: 8.310826,
+    name: "Main Station",
+    Address: "Zentralstrasse 1, Luzern",
+  },
+]
