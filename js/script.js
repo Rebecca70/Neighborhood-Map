19406358fdd867acc6bd5a85dd06cb327c5040c8
Rebecca Lötscher <rebeccaloetscher@hotmail.com>
2018-01-21 21:37:58 +0100

    docs: add locations to locations array

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-var initalLocation = {
-  lat: 47.050793,
-  lng: 8.310826,
-  content: "Main Station"
-}
+
 
 var locations = [
   {
-    lat: 47.050793,
-    lng: 8.310826,
-    name: "Main Station",
-    Address: "Zentralstrasse 1, Luzern",
+    location: {
+      lat: 47.052122,
+      lng: 8.309038
+    },
+    name: "Seebrücke",
+    address: "Seebrücke, Luzern"
   },
+  {
+    location: {
+      lat: 47.050793,
+      lng: 8.310826
+    },
+    name: "Main Station",
+    address: "Zentralstrasse 1, Luzern"
+  },
+  {
+    location: {
+      lat: 47.05162,
+      lng: 8.307509
+    },
+    name: "Kapellbrücke",
+    address: "Kapellbrücke, Luzern"
+  },
+  {
+    location: {
+      lat: 47.052873,
+      lng: 8.336391
+    },
+    name: "Verkehrshaus/Transport Museum",
+    address: "Lidostrasse 5, 6006 Luzern"
+  }
 ]
